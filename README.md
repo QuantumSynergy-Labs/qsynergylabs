0d77f728fa18226477cf6cf29fa20fc5ac9f08bc
CarlR <romales.136490120245@depedqc.ph>
2023-09-09 15:19:31 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# QuantumnSynergy Labs CLI
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Table of Contents
+
+- [Description](#description)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+- [License](#license)
+
+## Description
+
+The QuantumnSynergy Labs CLI is a command-line tool that simplifies the process of creating and setting up a new Discord bot. It automates the initial setup, including creating a bot user, obtaining a token, and generating a basic project structure.
+
+## Features
+
+- Easily create a new Discord bot with a few simple commands.
+- Automates the process of generating a bot token.
+- Provides a basic project structure to get you started.
+- Supports custom configurations for your bot.
+
+## Installation
+
+To use the QuantumnSynergy Labs CLI, you need to have Node.js and npm (Node Package Manager) installed on your system. If you haven't already, you can download and install them from [nodejs.org](https://nodejs.org/).
+
+Once Node.js and npm are installed, you can install the CLI tool globally using the following command:
+
+```bash
+npm install -g test-qsynergy
+npx test-qsynergy
---
+++ index.js
@@ -0,0 +1,169 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer';
+import fs from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+
+function getTimestamp() {
+    const currentDate = new Date();
+    return currentDate.toLocaleTimeString();
+}
+
+const introQuestions = [
+    {
+        type: 'list',
+        name: 'process',
+        message: 'Welcome to the QuantumSynergy Labs MGUI bot setup. What would you like to do today?',
+        choices: [
+            {
+                name: 'Start discord bot',
+                value: 'start'
+            },
+            {
+                name: 'Install bot dependencies',
+                value: 'install'
+            },
+            {
+                name: 'Create a new bot',
+                value: 'new'
+            },
+            {
+                name: 'Delete current bot',
+                value: 'delete'
+            },
+            {
+                name: 'Exit',
+                value: 'exit'
+            }
+        ]
+    }
+]
+
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your discord bot name?'
+    },
+    {
+        type: 'input',
+        name: 'token',
+        message: 'What is your discord bot token?'
+    },
+    {
+        type: 'input',
+        name: 'clientID',
+        message: 'What is your discord bot client ID?'
+    },
+    {
+        type: 'input',
+        name: 'devID',
+        message: 'What is your discord bot developer user id?'
+    },
+    {
+        type: 'input',
+        name: 'mongoURI',
+        message: 'What is your mongoDB URI (how to get in our docs)?'
+    },
+    {
+        type: 'input',
+        name: 'customerID',
+        message: 'What is your customer role id?'
+    },
+    {
+        type: 'input',
+        name: 'testServer',
+        message: 'What is your test server id?'
+    },
+    {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Are the above configurations correct?',
+        default: true
+    }
+];
+
+const __dirname = path.resolve();
+
+console.log(`----------------------------------------------------------------------------------------------------------------------------------------------`);
+console.log(`
+      ██████╗ ██╗   ██╗ █████╗ ███╗   ██╗████████╗██╗   ██╗███╗   ███╗███████╗██╗   ██╗███╗   ██╗███████╗██████╗  ██████╗██╗   ██╗
+     ██╔═══██╗██║   ██║██╔══██╗████╗  ██║╚══██╔══╝██║   ██║████╗ ████║██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝██╔══██╗██╔════╝╚██╗ ██╔╝
+     ██║   ██║██║   ██║███████║██╔██╗ ██║   ██║   ██║   ██║██╔████╔██║███████╗ ╚████╔╝ ██╔██╗ ██║█████╗  ██████╔╝██║  ███╗╚████╔╝ 
+     ██║▄▄ ██║██║   ██║██╔══██║██║╚██╗██║   ██║   ██║   ██║██║╚██╔╝██║╚════██║  ╚██╔╝  ██║╚██╗██║██╔══╝  ██╔══██╗██║   ██║ ╚██╔╝  
+     ╚██████╔╝╚██████╔╝██║  ██║██║ ╚████║   ██║   ╚██████╔╝██║ ╚═╝ ██║███████║   ██║   ██║ ╚████║███████╗██║  ██║╚██████╔╝  ██║   
+      ╚══▀▀═╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝   ╚═╝   
+                                                                                                                                  
+                                             ██╗      █████╗ ██████╗ ███████╗                                                     
+                                             ██║     ██╔══██╗██╔══██╗██╔════╝                                                     
+                                             ██║     ███████║██████╔╝███████╗                                                     
+                                             ██║     ██╔══██║██╔══██╗╚════██║                                                     
+                                             ███████╗██║  ██║██████╔╝███████║                                                     
+                                             ╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝                                        
+[INFO]: This is the QuantumSynergy Labs MGUI bot setup. This will help you create a new bot, delete a bot, or start a bot.
+[SUPPORT]: support@quantumsynergylabs.tech
+[DOCS]: https://docs.quantumsynergylabs.tech
+[CREDITS]: All rights reserved. QuantumSynergy Labs 2023.
+----------------------------------------------------------------------------------------------------------------------------------------------`);
+inquirer.prompt(introQuestions).then(async (answers) => {
+    if (answers.process === 'start') {
+        exec('npm run start', (err, stdout, stderr) => {
+            if (err) {
+               console.error(`\x1b[31m[${getTimestamp()}][ERROR]:\x1b[0m Error starting the bot`)
+            } else {
+                console.log(`\x1b[32m[SUCCESS]:\x1b[0m Bot has started successfully.`)
+            }
+        });
+    } else if (answers.process === 'install') {
+        exec('npm install', (err, stdout, stderr) => {
+            if (err) {
+               console.error(`\x1b[31m[${getTimestamp()}][ERROR]:\x1b[0m Error installing dependencies.`)
+            } else {
+                console.log(`\x1b[32m[SUCCESS]:\x1b[0m Dependencies installed.`)
+            }
+        });
+    } else if (answers.process === 'new') {
+        if (fs.existsSync(path.join(__dirname, 'config.json'))) {
+            console.log('Configurations already exist. You can start the bot.');
+            process.exit(0);
+        } else {
+            inquirer.prompt(questions).then(answers => {
+                if (answers.confirm) {
+                    const newConfig = {
+                        name: answers.name,
+                        token: answers.token,
+                        clientID: answers.clientID,
+                        devID: answers.devID,
+                        mongoURI: answers.mongoURI,
+                        customerID: answers.customerID,
+                        testServer: answers.testServer
+                    };
+                    fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(newConfig, null, 4));
+
+                    // Git clone command
+                    const command = `git clone https://github.com/notcarlrdev/notcarlrdev.git`
+                    exec(command, (error, stdout, stderr) => {
+                        if (error) {
+                            console.error(`\x1b[31m[${getTimestamp()}][ERROR]:\x1b[0m Error cloning the repo. Please try again.`);
+                        } else {
+                            console.log(`\x1b[32m[SUCCESS]:\x1b[0m Repository cloned successfully.`);
+                        }
+                    })
+                    console.log('\x1b[32m[SUCCESS]:\x1b[0m Configurations saved. You can now run the bot.');
+                } else {
+                    console.log('\x1b[31m[${getTimestamp()}][FAILED]:\x1b[0m Configurations not saved.');
+                }
+            });
+        }
+    } else if (answers.process === 'delete') {
+        if (fs.existsSync(path.join(__dirname, 'config.json'))) {
+            fs.unlinkSync(path.join(__dirname, 'config.json'));
+            console.log('\x1b[32m[SUCCESS]:\x1b[0m Configurations deleted. You can now create a new bot.');
+        } else {
+            console.log('\x1b[31m[${getTimestamp()}][FAILED]:\x1b[0m Configurations do not exist. You can create a new bot.');
+        }
+    } else if (answers.process === 'exit') {
+        process.exit();
+    }
+});
